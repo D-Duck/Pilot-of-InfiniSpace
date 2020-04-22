76de015d883a0555e44bc657953447cf4bbd973e
D. Duck <eerriikk1212@gmail.com>
2020-04-22 04:44:52 +0200

    commands v0.3
    + added basic commands
    + added fuel / hyper jump fogic
    + added limited range for small and hyper jump
    + added drawing of range of small jump
    * redone error printing

--- PilotOfInfiniSpace.py
+++ PilotOfInfiniSpace.py
@@ -2,7 +2,12 @@ import EGraphics as eg
 import random
 import time
 import math
-from keyboard import is_pressed
+from keyboard import is_pressed, get_hotkey_name
+
+# global data                                                                                                           Global data
+command = None
+commandTimeout = 0
+commandBlock = False
 
 # world settings                                                                                                        World settings
 blockSize = 50
@@ -28,8 +33,17 @@ globalX, globalY = 0, 0
 #player data                                                                                                            player data
 rot = 0
 poz = [0, 0]
+fuel = 10
+showRange = False
+showRangeTiming = time.time()
+hyperCharge = True
+hyperRange = 10000
+hyperChargeTiming = 0
 drawSolarSystem = False
 location_data = [0, 0]
+
+hyperRecovery = 300
+launchRange = 300
 
 # local seeder                                                                                                          f()   =   local seeder
 def local_seed(x, y):
@@ -87,7 +101,7 @@ def draw_background(win):
     for star in background:
         eg.draw_pixel(win, eg.color.white, star[0], star[1])
 def draw_frame(win):
-    global quickSettingsD, poz, target, rot, cicle, drawScene
+    global quickSettingsD, poz, target, rot, cicle, drawScene, hyperChargeTiming, hyperCharge, hyperRecovery, launchRange, showRange
 
     if drawScene == 0: # draw space
         draw_background(win)
@@ -108,6 +122,23 @@ def draw_frame(win):
             rot += math.pi * 2
         length = int(round(local_seed(poz[0], poz[1]) ** 25 * 25, 0)) + 10
         eg.draw_circle(win, eg.color.sky_blue, int(round(math.cos(rot) * length + poz[0] - globalX, 0)), int(round(math.sin(rot) * length + poz[1] - globalY, 0)), 5)
+        x, y, color = width - 25, height - 60, [255, 0, 0]
+        if showRange:
+            eg.draw_circle(window, eg.color.green, poz[0] - globalX, poz[1] - globalY, launchRange, 2)
+        for n in range(fuel + 1):
+            if n < fuel:
+                eg.draw_rectangle(win, color, x, y, 10, 20, draw_offset=(0, 0))
+                x -= 12
+                color[0] -= 25.5
+                color[1] += 25.55
+            else:
+                if hyperCharge:
+                    eg.draw_rectangle(win, eg.color.light_blue, width - 133, height - 35, 118, 20,draw_offset=(0, 0))
+                    eg.draw_rectangle(win, eg.color.white, width - 133, height - 35, 118, 20, 2,draw_offset=(0, 0))
+                else:
+                    eg.draw_rectangle(win, eg.color.light_blue, width - 132, height - 35,int(round((time.time() - hyperChargeTiming) * 118 / hyperRecovery, 0)), 20,draw_offset=(0, 0))
+                    eg.draw_rectangle(win, eg.color.gray, width - 133, height - 35, 118, 20, 2, draw_offset=(0, 0))
+
     elif drawScene == 1:
         draw_background(win)
         eg.draw_rectangle(win, eg.color.black, 50, 50, width - 100, height - 100, draw_offset=(0, 0))
@@ -119,23 +150,24 @@ def draw_frame(win):
             eg.draw_circle(win, float_to_rgb(int(str(local_value)[n + 4]), False), x, y, int(str(local_value)[n + 4]) * 2, 0)
             x += 55
     else:
-        print(f"Scene number {drawScene} does not exist. Changing it to 0")
+        print(f">>> Scene number {drawScene} does not exist. Changing it to 0\n")
         drawScene = 50
         
     # draw left click menu
     if leftClick: # adding menu buttons
         x, y = leftClickPoz
         if int(round(poz[0] / blockSize, 0)) != int(round((globalX + leftClickPoz[0]) / blockSize, 0)) or int(round(poz[1] / blockSize, 0)) != int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
-            if local_seed(globalX + x, globalY + y) > 1 - quickSettingsD[1]:
-                mx, my = eg.get_mouse_poz()
-                if x < mx < x + 75 and y < my < y + 25 :
-                    eg.draw_rectangle(win, eg.color.white, x, y, 75, 25, draw_offset=(0, 0))
-                    eg.draw_text(win, eg.color.black, x + 3, y + 5, "LAUNCH", 20, font="consolas")
-                else:
-                    eg.draw_rectangle(win, eg.color.black, x, y, 75, 25, draw_offset=(0, 0))
-                    eg.draw_text(win, eg.color.white, x + 3, y + 5, "LAUNCH", 20, font="consolas")
-                eg.draw_rectangle(win, eg.color.blue, x, y, 75, 25, draw_offset=(0, 0), border_width=1)
-                y += 25
+            if launchRange > math.sqrt((abs(globalX + leftClickPoz[0] - poz[0]) ** 2) + (abs(globalY + leftClickPoz[1] - poz[1]) ** 2)):
+                if local_seed(globalX + x, globalY + y) > 1 - quickSettingsD[1]:
+                    mx, my = eg.get_mouse_poz()
+                    if x < mx < x + 75 and y < my < y + 25 :
+                        eg.draw_rectangle(win, eg.color.white, x, y, 75, 25, draw_offset=(0, 0))
+                        eg.draw_text(win, eg.color.black, x + 3, y + 5, "LAUNCH", 20, font="consolas")
+                    else:
+                        eg.draw_rectangle(win, eg.color.black, x, y, 75, 25, draw_offset=(0, 0))
+                        eg.draw_text(win, eg.color.white, x + 3, y + 5, "LAUNCH", 20, font="consolas")
+                    eg.draw_rectangle(win, eg.color.blue, x, y, 75, 25, draw_offset=(0, 0), border_width=1)
+                    y += 25
         if int(round(poz[0] / blockSize, 0)) == int(round((globalX + leftClickPoz[0]) / blockSize, 0)) and int(round(poz[1] / blockSize, 0)) == int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
             mx, my = eg.get_mouse_poz()
             if x < mx < x + 75 and y < my < y + 25 :
@@ -145,10 +177,33 @@ def draw_frame(win):
                 eg.draw_rectangle(win, eg.color.black, x, y, 75, 25, draw_offset=(0, 0))
                 eg.draw_text(win, eg.color.white, x + 3, y + 5, "SOLAR", 20, font="consolas")
             eg.draw_rectangle(win, eg.color.blue, x, y, 75, 25, draw_offset=(0, 0), border_width=1)
+            y += 25
+        if hyperRange > math.sqrt((abs(globalX + leftClickPoz[0] - poz[0]) ** 2) + (abs(globalY + leftClickPoz[1] - poz[1]) ** 2)) > launchRange + 1:
+            mx, my = eg.get_mouse_poz()
+            if x < mx < x + 75 and y < my < y + 25 :
+                eg.draw_rectangle(win, eg.color.white, x, y, 75, 25, draw_offset=(0, 0))
+                eg.draw_text(win, eg.color.black, x + 3, y + 5, "HYPER", 20, font="consolas")
+            else:
+                eg.draw_rectangle(win, eg.color.black, x, y, 75, 25, draw_offset=(0, 0))
+                eg.draw_text(win, eg.color.white, x + 3, y + 5, "HYPER", 20, font="consolas")
+            eg.draw_rectangle(win, eg.color.blue, x, y, 75, 25, draw_offset=(0, 0), border_width=1)
+            y += 25
+        if int(round(poz[0] / blockSize, 0)) == int(round((globalX + leftClickPoz[0]) / blockSize, 0)) and int(round(poz[1] / blockSize, 0)) == int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
+            mx, my = eg.get_mouse_poz()
+            if x < mx < x + 75 and y < my < y + 25 :
+                eg.draw_rectangle(win, eg.color.white, x, y, 75, 25, draw_offset=(0, 0))
+                eg.draw_text(win, eg.color.black, x + 3, y + 5, "RANGE", 20, font="consolas")
+            else:
+                eg.draw_rectangle(win, eg.color.black, x, y, 75, 25, draw_offset=(0, 0))
+                eg.draw_text(win, eg.color.white, x + 3, y + 5, "RANGE", 20, font="consolas")
+            eg.draw_rectangle(win, eg.color.blue, x, y, 75, 25, draw_offset=(0, 0), border_width=1)
+            y += 25
 
     # draw text
     eg.draw_text(win, (255, 255, 255), 0, 0, f"{globalX} X {globalY}", 15)
-    eg.draw_text(win, (255, 255, 255), width - 30, height - 15, f"{cicle[2]}", 15)
+    eg.draw_text(win, (255, 255, 255), width - 30, 0, f"{cicle[2]}", 15)
+    if command != None:
+        eg.draw_text(win, (255, 255, 255), 0, height - 40, f"<<< {command}")
 
     eg.update()
 
@@ -156,35 +211,116 @@ def draw_frame(win):
 speed = 10
 multiplayer = 4
 quickSettingsK = [speed, multiplayer]
+def command_comparing(str1, str2):
+    same = True
+    try:
+        for n in range(len(str2)):
+            if str1[n].lower() != str2[n].lower():
+                same = False
+                break
+    except:
+        same = False
+    return same
+def command_parsing(str):
+    meta = []
+    meta_ = ""
+    if str[-1] != " ":
+        str = str + " "
+    for char in str:
+        if char == " ":
+            meta.append(meta_)
+            meta_ = ""
+        else:
+            meta_ += char
+    return meta
 def key_press_handler():
-    global quickSettingsK
+    global quickSettingsK, command, commandTimeout, commandBlock, globalY, globalX, fuel, hyperCharge
+
+    if is_pressed("`"):
+        if time.time() - commandTimeout > 0.5:
+            if command == None:
+                command = ""
+                commandTimeout = time.time()
+            else:
+                command = None
+                commandTimeout = time.time()
+            commandBlock = True
+    if command != None:
+        try:
+            key = get_hotkey_name()[0]
+            if not commandBlock:
+                if get_hotkey_name() == "backspace":
+                    command_ = ""
+                    for n in range(len(command) - 1):
+                        command_ += command[n]
+                    command = command_
+                elif get_hotkey_name() == "space":
+                    command += " "
+                    commandBlock = True
+                elif get_hotkey_name() == "enter": # commands here
+                    if command_comparing(command, "tp to"):
+                        backup = [globalX, globalY]
+                        try:
+                            globalX = int(command_parsing(command)[2])
+                            globalY = int(command_parsing(command)[3])
+                        except:
+                            globalX = backup[0]
+                            globalY = backup[1]
+                            print(f'Wierd coordinates for "tp to" ({command_parsing(command)[2]} X {command_parsing(command)[3]})')
+                    elif command_comparing(command, "tp player"):
+                        globalX = poz[0] - int(width / 2)
+                        globalY = poz[1] - int(height / 2)
+                    elif command_comparing(command, "set fuel"):
+                        ammount = int(command_parsing(command)[2])
+                        if ammount > 10:
+                            ammount = 10
+                        if ammount < 0:
+                            ammount = 0
+                        fuel = ammount
+                    elif command_comparing(command, "help"):
+                        print("hyper full = fully recharge hyper jump")
+                        print("set fuel N = set fuel to max N(0 - 10)")
+                        print(">>> tp to X Y = tp to exact coordinates given by player (X, Y)")
+                        print(">>> tp player = tp to player coordinates")
+                        print(">>> help = list all commands\n")
+                    elif command_comparing(command, "hyper full"):
+                        hyperCharge = True
+                    else:
+                        print(f'>>> Unknown command "{command}"\n')
+                    command = None
+                else:
+                    command += key
+                commandBlock = True
+        except:
+            commandBlock = False
 
     if is_pressed("esc"):
-        print("game was closed with esc")
+        print(">>> game was closed with esc")
         quit()
 
     x, y = 0, 0
-    if is_pressed("w"):
-        if is_pressed("shift"):
-            y -= quickSettingsK[0] * quickSettingsK[1]
-        else:
-            y -= quickSettingsK[0]
-    elif is_pressed("s"):
-        if is_pressed("shift"):
-            y += quickSettingsK[0] * quickSettingsK[1]
-        else:
-            y += quickSettingsK[0]
+    if command == None:
+        if is_pressed("w"):
+            if is_pressed("shift"):
+                y -= quickSettingsK[0] * quickSettingsK[1]
+            else:
+                y -= quickSettingsK[0]
+        elif is_pressed("s"):
+            if is_pressed("shift"):
+                y += quickSettingsK[0] * quickSettingsK[1]
+            else:
+                y += quickSettingsK[0]
 
-    if is_pressed("a"):
-        if is_pressed("shift"):
-            x -= quickSettingsK[0] * quickSettingsK[1]
-        else:
-            x -= quickSettingsK[0]
-    elif is_pressed("d"):
-        if is_pressed("shift"):
-            x += quickSettingsK[0] * quickSettingsK[1]
-        else:
-            x += quickSettingsK[0]
+        if is_pressed("a"):
+            if is_pressed("shift"):
+                x -= quickSettingsK[0] * quickSettingsK[1]
+            else:
+                x -= quickSettingsK[0]
+        elif is_pressed("d"):
+            if is_pressed("shift"):
+                x += quickSettingsK[0] * quickSettingsK[1]
+            else:
+                x += quickSettingsK[0]
 
     return x, y
 
@@ -196,6 +332,10 @@ while True:
     if time.time() - fpsTiming > fpsTarget:
         fpsTiming = time.time()
 
+        if not hyperCharge:
+            if time.time() - hyperChargeTiming >= hyperRecovery:
+                hyperCharge = True
+                hyperChargeTiming = 0
         # KEY PRESS HANDLER                                                                                            > Key press handler
         if drawScene == 0:
             metaX, metaY = key_press_handler()
@@ -210,20 +350,53 @@ while True:
                 if leftClick:
                     x, y = leftClickPoz
                     if int(round(poz[0] / blockSize, 0)) != int(round((globalX + leftClickPoz[0]) / blockSize, 0)) or int(round(poz[1] / blockSize, 0)) != int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
-                        if local_seed(globalX + x, globalY + y) > 1 - quickSettingsD[1]:
-                            mx, my = eg.get_mouse_poz()
-                            if x < mx < x + 75 and y < my < y + 25:
-                                poz = [int(round((globalX + leftClickPoz[0]) / blockSize, 0)) * blockSize, int(round((globalY + leftClickPoz[1]) / blockSize, 0)) * blockSize]
-                                leftClick = False
-                            y += 25
+                        if launchRange > math.sqrt((abs(globalX + leftClickPoz[0] - poz[0]) ** 2) + (abs(globalY + leftClickPoz[1] - poz[1]) ** 2)):
+                            if local_seed(globalX + x, globalY + y) > 1 - quickSettingsD[1]:
+                                if fuel > 0:
+                                    mx, my = eg.get_mouse_poz()
+                                    if x < mx < x + 75 and y < my < y + 25:
+                                        poz = [int(round((globalX + leftClickPoz[0]) / blockSize, 0)) * blockSize, int(round((globalY + leftClickPoz[1]) / blockSize, 0)) * blockSize]
+                                        leftClick = False
+                                        fuel -= 1
+                                    y += 25
                     if int(round(poz[0] / blockSize, 0)) == int(round((globalX + leftClickPoz[0]) / blockSize, 0)) and int(round(poz[1] / blockSize, 0)) == int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
                         mx, my = eg.get_mouse_poz()
                         if x < mx < x + 75 and y < my < y + 25:
                             drawScene = 1
                             leftClick = False
+                        y += 25
+                    if hyperRange > math.sqrt((abs(globalX + leftClickPoz[0] - poz[0]) ** 2) + (abs(globalY + leftClickPoz[1] - poz[1]) ** 2)) > launchRange + 1:
+                        mx, my = eg.get_mouse_poz()
+                        if x < mx < x + 75 and y < my < y + 25:
+                            if int(round(poz[0] / blockSize, 0)) != int(round((globalX + leftClickPoz[0]) / blockSize, 0)) or int(round(poz[1] / blockSize, 0)) != int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
+                                if local_seed(globalX + leftClickPoz[0], globalY + leftClickPoz[1]) > 1 - quickSettingsD[1]:
+                                    if hyperCharge:
+                                        if x < mx < x + 75 and y < my < y + 25:
+                                            poz = [int(round((globalX + leftClickPoz[0]) / blockSize, 0)) * blockSize,int(round((globalY + leftClickPoz[1]) / blockSize, 0)) * blockSize]
+                                            leftClick = False
+                                            hyperCharge = False
+                                            hyperChargeTiming = time.time()
+                            leftClick = False
+                        y += 25
+                    if int(round(poz[0] / blockSize, 0)) == int(round((globalX + leftClickPoz[0]) / blockSize, 0)) and int(round(poz[1] / blockSize, 0)) == int(round((globalY + leftClickPoz[1]) / blockSize, 0)):
+                        mx, my = eg.get_mouse_poz()
+                        if x < mx < x + 75 and y < my < y + 25:
+                            if showRange:
+                                showRange = False
+                            else:
+                                showRange = True
+                            showRangeTiming = time.time()
+                            leftClick = False
+                        y += 25
+
                     if not leftClick:
                         location_data = leftClickPoz
                         leftClickPoz = [0, 0]
+                    else:
+                        leftClick = False
+                        location_data = leftClickPoz
+                        leftClickPoz = [0, 0]
+
 
             if eg.get_mouse_click()[2]:
                 mx, my = eg.get_mouse_poz()
